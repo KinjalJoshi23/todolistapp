4ca755e78a4f94123a68b03dab47ae439a33bbdc
Kinjal Joshi <kinjal.joshi0905@gmail.com>
2026-07-20 13:52:25 +0530

    fix: fix in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"/todolistapp/",
+  base:"/",
   plugins: [react(), tailwindcss()],
 })
